21c126cfe56e9b7c5fdce73770defa9feac09e11
Melvrick <melvrickgoh@hotmail.com>
2013-09-26 06:07:15 +0800

    r to be conflicted

--- IDP/gen/com/idp/R.java
+++ IDP/gen/com/idp/R.java
@@ -9,7 +9,7 @@ package com.idp;
 
 public final class R {
     public static final class array {
-        public static final int events=0x7f050000;
+        public static final int events=0x7f060000;
     }
     public static final class attr {
     }
@@ -23,8 +23,8 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
         public static final int app_background=0x7f020000;
@@ -36,10 +36,13 @@ public final class R {
         public static final int ic_launcher=0x7f020006;
     }
     public static final class id {
-        public static final int action_search=0x7f0a0003;
-        public static final int action_settings=0x7f0a0002;
+        public static final int action_search=0x7f0a0006;
+        public static final int action_settings=0x7f0a0005;
+        public static final int datePicker1=0x7f0a0003;
+        public static final int editText1=0x7f0a0002;
         public static final int pager=0x7f0a0000;
         public static final int section_label=0x7f0a0001;
+        public static final int timePicker1=0x7f0a0004;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -52,13 +55,16 @@ public final class R {
         public static final int main=0x7f090000;
     }
     public static final class string {
-        public static final int action_search=0x7f060005;
-        public static final int action_settings=0x7f060001;
-        public static final int app_name=0x7f060000;
-        public static final int janan=0x7f060006;
-        public static final int title_section1=0x7f060002;
-        public static final int title_section2=0x7f060003;
-        public static final int title_section3=0x7f060004;
+        public static final int action_search=0x7f040008;
+        public static final int action_settings=0x7f040004;
+        public static final int add_activity=0x7f040003;
+        public static final int app_name=0x7f040002;
+        public static final int dialog_activity_date=0x7f040000;
+        public static final int dialog_activity_time=0x7f040001;
+        public static final int janan=0x7f040009;
+        public static final int title_section1=0x7f040005;
+        public static final int title_section2=0x7f040006;
+        public static final int title_section3=0x7f040007;
     }
     public static final class style {
         /** 
