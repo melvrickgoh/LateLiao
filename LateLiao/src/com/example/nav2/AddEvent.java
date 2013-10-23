676622ebcacda32d8a45c02ca48b37c379307d42
wynerlim <wynerlim@gmail.com>
2013-10-24 06:02:54 +0800

    Change UI, Fonts, Icons

--- LateLiao/src/com/example/nav2/AddEvent.java
+++ LateLiao/src/com/example/nav2/AddEvent.java
@@ -4,10 +4,13 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
+import java.util.HashMap;
+import java.util.List;
 
 import com.aws.AWSClientManager;
 
 import android.os.Bundle;
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.DatePickerDialog.OnDateSetListener;
@@ -17,8 +20,13 @@ import android.app.TimePickerDialog.OnTimeSetListener;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v4.app.ActionBarDrawerToggle;
 import android.support.v4.app.NavUtils;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -26,12 +34,18 @@ import android.view.ViewGroup;
 import android.view.View.OnClickListener;
 import android.view.View.OnTouchListener;
 import android.view.inputmethod.InputMethodManager;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.TimePicker;
+import android.widget.AdapterView.OnItemClickListener;
 
-public class AddEvent extends Activity implements OnDateSetListener, OnTimeSetListener, OnFriendsEventListener  {
+@SuppressLint("NewApi")
+public class AddEvent extends ActionBarActivity implements OnDateSetListener, OnTimeSetListener, OnFriendsEventListener  {
 	Button dateButton;
 	Button timeButton;
 	Button locationButton;
@@ -48,11 +62,30 @@ public class AddEvent extends Activity implements OnDateSetListener, OnTimeSetLi
 	private String eventTime = "";
 	private Location eventLocation = new Location("", 0, 0);
 	private ArrayList<String> eventFriends = new ArrayList<String>();
-
+	
+	private static final String STATE_SELECTED_NAVIGATION_ITEM = "selected_navigation_item";
+	
+	// Array of strings storing country names
+	String[] mOptions ;
+		 
+	// Array of integers points to images stored in /res/drawable-ldpi/
+	 int[] mLogos = new int[3];
+	 
+	 private DrawerLayout mDrawerLayout;
+	 private ListView mDrawerList;
+	 private ActionBarDrawerToggle mDrawerToggle;
+	 private LinearLayout mDrawer ;
+	 private List<HashMap<String,String>> mList ;
+	 private SimpleAdapter mAdapter;
+	 final private String IMAGEICON = "imageicon";
+	 final private String TABNAME = "tabname";
+		 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(this.savedInstanceState);
+		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_add_event);
+		
+		showActionBar();
 		
 		Bundle intentBundle = getIntent().getExtras();
 		this.currentUser = (User) intentBundle.get("user");
@@ -94,6 +127,71 @@ public class AddEvent extends Activity implements OnDateSetListener, OnTimeSetLi
 			this.setTitle(this.editEvent.getEventName());
 			updateButtonValues(this.editEvent,this.currentUser,dateButton,timeButton,locationButton,friendsButton);
 		}
+		
+		mLogos[0] = getUserIcon(this,currentUser);
+		mLogos[1] = R.drawable.add_event;
+		mLogos[2] =	R.drawable.logout;
+		
+		/* Side bar creation */
+	    // Getting an array of country names
+	    mOptions = getResources().getStringArray(R.array.options);
+	    mOptions[0] = currentUser.getName();
+	    
+	    // Getting a reference to the drawer listview
+	    mDrawerList = (ListView) findViewById(R.id.drawer_list);
+	    
+	    // Getting a reference to the sidebar drawer ( Title + ListView )
+	    mDrawer = ( LinearLayout) findViewById(R.id.drawer);
+	    
+	    // Each row in the list stores country name, count and flag
+	    mList = new ArrayList<HashMap<String,String>>();
+	    for(int i=0;i<3;i++){
+	        HashMap<String, String> hm = new HashMap<String,String>();
+	        hm.put(TABNAME, mOptions[i]);
+	        hm.put(IMAGEICON, Integer.toString(mLogos[i]) );
+	        mList.add(hm);
+	    }
+    
+	   // Keys used in Hashmap
+	    String[] from = { IMAGEICON, TABNAME };
+	    
+	   // Ids of views in listview_layout
+	    int[] to = { R.id.imageicon , R.id.tabname };
+    
+	   // Instantiating an adapter to store each items
+	    mAdapter = new SimpleAdapter(this, mList, R.layout.drawer_layout, from, to);
+	    
+	    // Getting reference to DrawerLayout
+	    mDrawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
+	    
+	    // Creating a ToggleButton for NavigationDrawer with drawer event listener
+	    mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.drawable.sidebar , R.string.drawer_open,R.string.drawer_close){
+    
+	        //** Called when drawer is closed *//*
+	        public void onDrawerClosed(View view) {
+		        invalidateOptionsMenu();
+	        }
+	    
+	       //** Called when a drawer is opened *//*
+	        public void onDrawerOpened(View drawerView) {
+	        	getSupportActionBar().setTitle("SideBar");
+	        	invalidateOptionsMenu();
+	        }
+	    };
+    
+	    // Setting event listener for the drawer
+	    mDrawerLayout.setDrawerListener(mDrawerToggle);
+	    
+	    // ItemClick event handler for the drawer items
+	    mDrawerList.setOnItemClickListener(drawerListener);
+    
+	    // Enabling Up navigation
+	    getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+	    getSupportActionBar().setDisplayShowHomeEnabled(true);
+    
+	   // Setting the adapter to the listView
+	    mDrawerList.setAdapter(mAdapter);
+	    
 	}
 	
 	private void updateButtonValues(Event editEvent, User currentUser, Button dateButton, Button timeButton, Button locationButton,
@@ -246,19 +344,73 @@ public class AddEvent extends Activity implements OnDateSetListener, OnTimeSetLi
             eventName.setVisibility(View.VISIBLE);
 	    }
 	};
-
+	
+	private OnItemClickListener drawerListener = new OnItemClickListener() {
+	    
+	       @Override
+	        public void onItemClick(AdapterView<?> arg0, View arg1, int position, long arg3) {
+	        
+	    	   if (position == 0) {
+	    	   		Intent intent = new Intent(getApplicationContext(),ProfileActivity.class);
+	                intent.putExtra("user", currentUser);
+	                startActivity(intent);
+	    	   		
+	    	   	}
+	    	   	else if (position == 1) {
+	    	   		Intent intent = new Intent(getApplicationContext(),AddEvent.class);
+	    	   		intent.putExtra("user", currentUser);
+	                startActivity(intent);
+	    	   		//do something
+	    	   	} else {	
+	    	   		Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+		        	intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+	                startActivity(intent);
+	    	   	}
+	        
+		        // Closing the drawer
+		        mDrawerLayout.closeDrawer(mDrawer);
+	       }
+	};
+	
+	private ActionBar showActionBar() {
+        LayoutInflater inflator = (LayoutInflater) this
+            .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        	View v = inflator.inflate(R.layout.custom, null);
+        	ActionBar actionBar = getSupportActionBar();
+		    actionBar.setDisplayHomeAsUpEnabled(true);
+		    actionBar.setDisplayShowHomeEnabled (false);
+		    actionBar.setDisplayShowCustomEnabled(true);
+		    actionBar.setDisplayShowTitleEnabled(false);
+		    actionBar.setCustomView(v);
+		    return actionBar;
+	}
+	
+	private int getUserIcon(Context context, User user){
+		 String imageLocation = user.getImageLocation();
+		 return context.getResources().getIdentifier(imageLocation.toLowerCase(), "drawable", context.getPackageName());
+	}
+	
+	@Override
+	public void onRestoreInstanceState(Bundle savedInstanceState) {
+		// Restore the previously serialized current tab position.
+		if (savedInstanceState.containsKey(STATE_SELECTED_NAVIGATION_ITEM)) {
+			getSupportActionBar().setSelectedNavigationItem(
+					savedInstanceState.getInt(STATE_SELECTED_NAVIGATION_ITEM));
+		}
+	}
+	
+	@Override
+	public void onSaveInstanceState(Bundle outState) {
+		// Serialize the current tab position.
+		outState.putInt(STATE_SELECTED_NAVIGATION_ITEM, getSupportActionBar()
+				.getSelectedNavigationIndex());
+	}
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.add_event, menu);
-		return true;
-	}
-
-	/**
-	 * Set up the {@link android.app.ActionBar}.
-	 */
-	private void setupActionBar() {
-		getActionBar().setDisplayHomeAsUpEnabled(true);
+		getMenuInflater().inflate(R.menu.user, menu);
+	     return super.onCreateOptionsMenu(menu);
 	}
 	
 	public static void hideSoftKeyboard(Activity activity) {
@@ -305,20 +457,19 @@ public class AddEvent extends Activity implements OnDateSetListener, OnTimeSetLi
 	
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
-		switch (item.getItemId()) {
-		case android.R.id.home:
-			// This ID represents the Home or Up button. In the case of this
-			// activity, the Up button is shown. Use NavUtils to allow users
-			// to navigate up one level in the application structure. For
-			// more details, see the Navigation pattern on Android Design:
-			//
-			// http://developer.android.com/design/patterns/navigation.html#up-vs-back
-			//
-			NavUtils.navigateUpFromSameTask(this);
+		if (mDrawerToggle.onOptionsItemSelected(item)) {
 			return true;
-		}
-		return super.onOptionsItemSelected(item);
+		 }
+		return false;
 	}
+	
+	 @Override
+	 protected void onPostCreate(Bundle savedInstanceState) {
+		 super.onPostCreate(savedInstanceState);
+		 mDrawerToggle.syncState();
+	 
+	 }
+	 
 	private ArrayList getListData(){
 		AWSClientManager aws = AWSClientManager.getInstance();		
 		return aws.getAllUsers();
